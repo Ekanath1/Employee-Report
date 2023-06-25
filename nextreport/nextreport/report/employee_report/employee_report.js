7952bec2469bd1872cc4411a1fc52878fb48ebe1
Ekanath1 <eknathbiradar551@gmail.com>
2023-06-25 12:15:28 +0530

    employee_attendance_report

---
+++ nextreport/nextreport/report/employee_report/employee_report.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2023, eknath and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employee Report"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": ("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd":1
+		},
+		{
+			"fieldname":"from_date",
+			"label": ("From Date"),
+			"fieldtype": "Date",
+			"reqd":1
+		},
+
+		{
+			"fieldname":"to_date",
+			"label": ("To Date"),
+			"fieldtype": "Date",
+			"reqd":1,
+			
+		},
+		{
+			"fieldname": "based_on",
+			"label": __("Based On"),
+			"fieldtype": "Select",
+			"options": [
+				{ "value": "Monthly", "label": __("Monthly") },
+				{ "value": "Yearly", "label": __("Yearly") }
+			],
+			"default": "Yearly",
+			"reqd": 1
+		},
+
+	]
+};
